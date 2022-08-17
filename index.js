e1a76af7bdbf85fc4c30fc91c2d464707f178ed0
SheeTabz <wmuriithitabitha@gmail.com>
2022-08-17 10:08:23 +0300

    Array methods practice

---
+++ index.js
@@ -0,0 +1,11 @@
+const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+const day = days.slice(2, 5);
+
+
+console.log(days, day);
+const num = [1,2,3,4,5,6,7,8,9,10,]
+// num.splice("Sheee")
+// console.log(num);
+const newNum = [...num.slice(0,3),"20","21"]
+console.log(newNum)
